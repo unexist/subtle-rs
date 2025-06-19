be544c79f4bdfc0337adef22a29bd7b0320fc035
Christoph Kappel <christoph@unexist.dev>
2025-06-19 17:14:58 +0200

    Added initial client handling

--- src/client.rs
+++ src/client.rs
@@ -10,15 +10,25 @@
 ///
 
 use x11rb::protocol::xproto::Window;
+use bitflags::bitflags;
 
-#[derive(Debug, Default)]
-struct Client {
-    win: Window,
-    title: String,
+bitflags! {
+    #[derive(Default, Debug)]
+    pub(crate) struct Flags: u32 {
+        const DEAD = 1 << 0;
+        const FOCUS = 1 << 1;
+    }
+}
+
+#[derive(Default, Debug)]
+pub(crate) struct Client {
+    pub flags: Flags,
+    pub win: Window,
+    pub title: String,
 }
 
 impl Client {
-    fn new(win: Window) -> Self {
+    pub(crate) fn new(win: Window) -> Self {
         Client {
             win,
             ..Self::default()
