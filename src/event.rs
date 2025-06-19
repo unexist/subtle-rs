0b16edb5208ee60204f13b70a1af22c58821b126
Christoph Kappel <christoph@unexist.dev>
2025-06-19 17:14:46 +0200

    Added exit handling of main loop

--- src/event.rs
+++ src/event.rs
@@ -9,9 +9,35 @@
 /// See the file LICENSE for details.
 ///
 
-use anyhow::{Result};
-use crate::Subtle;
+use anyhow::Result;
+use std::sync::atomic;
+use x11rb::connection::Connection;
+use x11rb::protocol::xproto::MapRequestEvent;
+use x11rb::protocol::Event;
+use crate::subtle::Subtle;
+use crate::client::Client;
 
-pub(crate) fn handle_requests(_subtle: &mut Subtle) -> Result<()> {
+fn handle_map_request(event: MapRequestEvent) -> Result<()> {
+    let client = Client::new(event.window);
+    
+    
+    Ok(())
+}
+
+pub(crate) fn handle_requests(subtle: &mut Subtle) -> Result<()> {
+    if let Some(conn) = subtle.conn.as_mut() {
+        while subtle.running.load(atomic::Ordering::SeqCst) {
+            conn.flush()?;
+
+            let event = conn.wait_for_event()?;
+
+            match event {
+                Event::MapRequest(event) => handle_map_request(event)?,
+
+                _ => println!("Unhandled event: {:?}", event),
+            }
+        }
+    }
+    
     Ok(())
 }
