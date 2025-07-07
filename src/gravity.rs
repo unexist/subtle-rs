bd8e772092676388236dd40e23d2fb25dc641162
Christoph Kappel <christoph@unexist.dev>
2025-07-07 12:33:01 +0200

    Added #publish for gravities

--- src/gravity.rs
+++ src/gravity.rs
@@ -15,7 +15,9 @@ use easy_min_max::{min, max, clamp};
 use anyhow::Result;
 use log::debug;
 use stdext::function_name;
-use x11rb::protocol::xproto::Rectangle;
+use x11rb::connection::Connection;
+use x11rb::protocol::xproto::{AtomEnum, PropMode, Rectangle};
+use x11rb::wrapper::ConnectionExt;
 use crate::Config;
 use crate::subtle::Subtle;
 
@@ -64,8 +66,32 @@ pub(crate) fn init(config: &Config, subtle: &mut Subtle) -> Result<()> {
     subtle.gravities = config.gravities.iter()
         .map(|grav| Gravity::new(String::from(grav.0), grav.1[0], grav.1[1], 
                                  grav.1[2], grav.1[3])).collect();
+    
+    publish(subtle)?;
 
     debug!("{}", function_name!());
     
     Ok(())
 }
+
+pub(crate) fn publish(subtle: &Subtle) -> Result<()> {
+    let conn = subtle.conn.get().unwrap();
+    let atoms = subtle.atoms.get().unwrap();
+
+    let screen = &conn.setup().roots[subtle.screen_num];
+    
+    let mut gravities: Vec<String> = Vec::with_capacity(subtle.gravities.len());
+
+    for gravity in subtle.gravities.iter() {
+        gravities.push(format!("{}x{}+{}+{}#{}", gravity.geom.x, gravity.geom.y,
+                               gravity.geom.width, gravity.geom.height, gravity.name));
+    }
+
+    conn.change_property8(PropMode::REPLACE, screen.root, atoms.SUBTLE_GRAVITY_LIST,
+                          AtomEnum::STRING, gravities.join("\0").as_bytes())?.check()?;
+
+    debug!("{}: gravities={}", function_name!(), subtle.gravities.len());
+
+    Ok(())
+}
+
