b8cb30221412cba9e8cc2280b8d3cdf21fe38e12
Christoph Kappel <christoph@unexist.dev>
2025-07-09 13:52:42 +0200

    Added stub for property events and updated logging

--- src/event.rs
+++ src/event.rs
@@ -1,3 +1,4 @@
+use std::ptr::fn_addr_eq;
 ///
 /// @package subtle-rs
 ///
@@ -12,9 +13,11 @@
 use anyhow::{Context, Result};
 use std::sync::atomic;
 use log::{debug, warn};
+use stdext::function_name;
 use x11rb::connection::Connection;
-use x11rb::protocol::xproto::{DestroyNotifyEvent, DestroyWindowRequest, ExposeEvent, MapRequestEvent, SelectionClearEvent, UnmapNotifyEvent};
+use x11rb::protocol::xproto::{DestroyNotifyEvent, DestroyWindowRequest, ExposeEvent, MapRequestEvent, PropertyNotifyEvent, SelectionClearEvent, UnmapNotifyEvent};
 use x11rb::protocol::Event;
+use x11rb::protocol::xinput::PropertyEvent;
 use crate::subtle::{SubtleFlags, Subtle};
 use crate::client::Client;
 use crate::screen;
@@ -24,11 +27,15 @@ fn handle_expose(subtle: &Subtle, event: ExposeEvent) {
         screen::render(subtle);    
     }
     
-    debug!("Expose: win={}", event.window);
+    debug!("{}: win={}, count={}", function_name!(), event.window, event.count);
 }
 
 fn handle_destroy(subtle: &Subtle, event: DestroyNotifyEvent) {
-    debug!("Destroy: win={}", event.window);
+    debug!("{}: win={}", function_name!(), event.window);
+}
+
+fn handle_property(subtle: &Subtle, event: PropertyNotifyEvent) {
+    debug!("{}: win={}", function_name!(), event.window);
 }
 
 fn handle_map_request(subtle: &Subtle, event: MapRequestEvent) {
@@ -41,11 +48,11 @@ fn handle_map_request(subtle: &Subtle, event: MapRequestEvent) {
         let _map_client = Client::new(subtle, event.window);
     }
 
-    debug!("MapRequest: win={}", event.window);
+    debug!("{}: win={}", function_name!(), event.window);
 }
 
 fn handle_unmap(subtle: &Subtle, event: UnmapNotifyEvent) {
-    debug!("Unmap: win={}", event.window);
+    debug!("{}: win={}", function_name!(), event.window);
 }
 
 fn handle_selection(subtle: &Subtle, event: SelectionClearEvent) {
@@ -56,8 +63,8 @@ fn handle_selection(subtle: &Subtle, event: SelectionClearEvent) {
         subtle.exterminate.store(false, atomic::Ordering::Relaxed);
     }
     
-    debug!("SelectionClear: win={}, tray={}, support={}",
-        event.owner, subtle.tray_win, subtle.support_win);
+    debug!("{}: win={}, tray={}, support={}",
+        function_name!(), event.owner, subtle.tray_win, subtle.support_win);
 }
 
 pub(crate) fn event_loop(subtle: &Subtle) -> Result<()> {
@@ -71,6 +78,7 @@ pub(crate) fn event_loop(subtle: &Subtle) -> Result<()> {
                 Event::Expose(evt) => handle_expose(subtle, evt),
                 Event::DestroyNotify(evt) => handle_destroy(subtle, evt),
                 Event::MapRequest(evt) => handle_map_request(subtle, evt),
+                Event::PropertyNotify(evt) => handle_property(subtle, evt),
                 Event::SelectionClear(evt) => handle_selection(subtle, evt),
                 Event::UnmapNotify(evt) => handle_unmap(subtle, evt),
 
