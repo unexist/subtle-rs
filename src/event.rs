9f767696828819ec03b4d39fb1940a18473b3ffb
Christoph Kappel <christoph@unexist.dev>
2025-07-10 15:39:08 +0200

    Updated handling of property changes

--- src/event.rs
+++ src/event.rs
@@ -18,7 +18,7 @@ use x11rb::protocol::xproto::{DestroyNotifyEvent, DestroyWindowRequest, EnterNot
 use x11rb::protocol::Event;
 use x11rb::protocol::xinput::{EnterEvent, PropertyEvent};
 use crate::subtle::{SubtleFlags, Subtle};
-use crate::client::Client;
+use crate::client::{Client, ClientFlags};
 use crate::screen;
 
 fn handle_enter(subtle: &Subtle, event: EnterNotifyEvent) {
@@ -48,7 +48,68 @@ fn handle_destroy(subtle: &Subtle, event: DestroyNotifyEvent) {
 }
 
 fn handle_property(subtle: &Subtle, event: PropertyNotifyEvent) {
-    debug!("{}: win={}", function_name!(), event.window);
+    let atoms = subtle.atoms.get().unwrap();
+
+    if atoms.WM_NAME == event.atom {
+        if let Some(mut client) = subtle.find_client_mut(event.window) {
+            let _ = client.set_wm_name(subtle);
+
+            if let Some(win) = subtle.focus_history.borrow(0)
+                && event.window == *win
+            {
+                screen::update(subtle);
+                screen::render(subtle);
+            }
+        }
+    } else if atoms.WM_NORMAL_HINTS == event.atom {
+        if let Some(mut client) = subtle.find_client_mut(event.window) {
+            let mut mode_flags = ClientFlags::empty();
+
+            let _ = client.set_size_hints(subtle, &mut mode_flags);
+
+            let enable_only = client.flags.complement().intersection(mode_flags);
+            let _ = client.toggle(&enable_only, true);
+
+            if client.is_visible(subtle) {
+                screen::update(subtle);
+                screen::render(subtle);
+            }
+
+        }
+        // TODO tray
+    } else if atoms.WM_HINTS == event.atom {
+        if let Some(mut client) = subtle.find_client_mut(event.window) {
+            let mut mode_flags = ClientFlags::empty();
+
+            let _ = client.set_wm_hints(subtle, &mut mode_flags);
+
+            let enable_only = client.flags.complement().intersection(mode_flags);
+            let _ = client.toggle(&enable_only, true);
+
+            if client.is_visible(subtle) || client.flags.contains(ClientFlags::MODE_URGENT) {
+                screen::update(subtle);
+                screen::render(subtle);
+            }
+        }
+    } else if atoms._NET_WM_STRUT == event.atom {
+        if let Some(mut client) = subtle.find_client_mut(event.window) {
+            let _ = client.set_strut(subtle);
+
+            screen::update(subtle);
+        }
+    } else if atoms._MOTIF_WM_HINTS == event.atom {
+        if let Some(mut client) = subtle.find_client_mut(event.window) {
+            let mut mode_flags = ClientFlags::empty();
+
+            let enable_only = client.flags.complement().intersection(mode_flags);
+            client.toggle(&enable_only, true);
+
+            let _= client.set_motif_wm_hints(subtle, &mut mode_flags);
+        }
+    }
+    // TODO tray
+
+    debug!("{}: win={}, atom={}", function_name!(), event.window, event.atom);
 }
 
 fn handle_map_request(subtle: &Subtle, event: MapRequestEvent) {
@@ -70,7 +131,7 @@ fn handle_unmap(subtle: &Subtle, event: UnmapNotifyEvent) {
 
 fn handle_selection(subtle: &Subtle, event: SelectionClearEvent) {
     if event.owner == subtle.tray_win {
-       debug!("Tray not supported yet"); 
+        debug!("Tray not supported yet");
     } else if event.owner == subtle.support_win {
         warn!("Leaving the field");
         subtle.exterminate.store(false, atomic::Ordering::Relaxed);
