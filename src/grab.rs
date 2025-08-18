b23a9c55efa3b54382c65ab611465e4453498b6b
Christoph Kappel <christoph@unexist.dev>
2025-08-18 14:52:10 +0200

    Added #set and #unset

--- src/grab.rs
+++ src/grab.rs
@@ -11,15 +11,17 @@
 
 use std::fmt;
 use bitflags::bitflags;
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, Context, Result};
 use log::debug;
 use stdext::function_name;
-use x11rb::protocol::xproto::ModMask;
+use x11rb::connection::Connection;
+use x11rb::NONE;
+use x11rb::protocol::xproto::{ButtonIndex, ConnectionExt, EventMask, GrabMode, Keycode, ModMask, Window};
 use crate::config::Config;
-use crate::subtle::Subtle;
+use crate::subtle::{Subtle, SubtleFlags};
 
 bitflags! {
-    #[derive(Default, Debug)]
+    #[derive(Default, Debug, Copy, Clone)]
     pub(crate) struct GrabFlags: u32 {
         const KEY = 1 << 0; // Key grab
         const MOUSE = 1 << 1; // Mouse grab
@@ -59,8 +61,8 @@ bitflags! {
 pub(crate) struct Grab {
     pub(crate) flags: GrabFlags,
 
-    pub(crate) code: u16,
-    pub(crate) state: u16,
+    pub(crate) code: Keycode,
+    pub(crate) modifiers: ModMask,
 
     pub(crate) app: Option<String>,
 }
@@ -72,38 +74,38 @@ const XK_Pointer_Button1: u32 = 0xfee9;
 const XK_a: u32 = 0x0061;
 
 #[doc(hidden)]
-pub(crate) fn parse_keys(keys: &str) -> Result<(u32, u32, u32, bool)> {
+pub(crate) fn parse_keys(keys: &str) -> Result<(u32, Keycode, ModMask, bool)> {
     let mut sym = 0u32;
-    let mut code = 0u32;
-    let mut state = ModMask::default();
+    let mut code: Keycode = 0;
+    let mut modifiers = ModMask::default();
     let mut is_mouse = false;
 
     for key in keys.split("-") {
         match key {
-            "S" => state |= ModMask::SHIFT,
-            "C" => state |= ModMask::CONTROL,
-            "A" => state |= ModMask::M1,
-            "M" => state |= ModMask::M3,
-            "W" => state |= ModMask::M4,
-            "G" => state |= ModMask::M5,
+            "S" => modifiers |= ModMask::SHIFT,
+            "C" => modifiers |= ModMask::CONTROL,
+            "A" => modifiers |= ModMask::M1,
+            "M" => modifiers |= ModMask::M3,
+            "W" => modifiers |= ModMask::M4,
+            "G" => modifiers |= ModMask::M5,
             _ => {
                 if key.starts_with("B") {
                     let (_, btn) = key.split_at(1);
 
                     sym = XK_Pointer_Button1;
-                    code = XK_Pointer_Button1 + btn.parse::<u32>()?;
+                    code = btn.parse::<Keycode>()?;
                     is_mouse = true;
                 }
             }
         }
     }
 
-    Ok((sym, code, u32::from(state), is_mouse))
+    Ok((sym, code, modifiers, is_mouse))
 }
 
 #[doc(hidden)]
 pub(crate) fn parse_name(name: &str) -> Result<GrabFlags> {
-    let flag = match name {
+    Ok(match name {
         "view_focus" => GrabFlags::VIEW_FOCUS,
         "view_swap" => GrabFlags::VIEW_SWAP,
         "view_select" => GrabFlags::VIEW_SELECT,
@@ -120,17 +122,11 @@ pub(crate) fn parse_name(name: &str) -> Result<GrabFlags> {
         "window_select" => GrabFlags::WINDOW_SELECT,
         "window_gravity" => GrabFlags::WINDOW_GRAVITY,
         "window_kill" => GrabFlags::WINDOW_KILL,
-        _ => {
-            GrabFlags::empty()
-        }
-    };
-
-    if flag.is_empty() {
-        Err(anyhow!("Grab not found: {}", name))
-    } else {
-        Ok(flag)
-    }
+        _ => return Err(anyhow!("Grab not found: {}", name))
+    })
 }
+
+//const MASK_STATES: Vec<i32> = vec![0, ModMask::LOCK, numlockmask, numlockmask | ModMask::LOCK];
 
 
 impl Grab {
@@ -138,12 +134,12 @@ impl Grab {
 
         // Parse name and keys
         let key_flag = parse_name(name)?;
-        let (sym, code, state, is_mouse) = parse_keys(keys)?;
+        let (sym, code, modifiers, is_mouse) = parse_keys(keys)?;
 
         let mut grab = Grab {
             flags: key_flag,
-            state: state as u16,
-            ..Self::default()
+            modifiers: modifiers,
+            ..Default::default()
         };
 
         if is_mouse {
@@ -156,20 +152,81 @@ impl Grab {
 
         Ok(grab)
     }
+
+    pub(crate) fn set(subtle: Subtle, win: Window, grab_mask: GrabFlags) -> Result<()> {
+        let conn = subtle.conn.get().context("Failed to get connection")?;
+
+        let default_screen = &conn.setup().roots[subtle.screen_num];
+
+        // Unbind click-to-focus grab
+        if subtle.flags.intersects(SubtleFlags::FOCUS_CLICK) && default_screen.root != win {
+            conn.ungrab_button(ButtonIndex::ANY, win, ModMask::ANY)?.check()?;
+        }
+
+        for grab in subtle.grabs.iter() {
+            if grab.flags.intersects(grab_mask) {
+                if grab.flags.intersects(GrabFlags::KEY) {
+                    conn.grab_key(true, win, grab.modifiers, grab.code,
+                                  GrabMode::ASYNC, GrabMode::ASYNC)?.check()?;
+                } else if grab.flags.intersects(GrabFlags::MOUSE) {
+                    conn.grab_button(false, win,
+                                     EventMask::BUTTON_PRESS | EventMask::BUTTON_RELEASE,
+                                     GrabMode::ASYNC, GrabMode::ASYNC, NONE, NONE,
+                                     ButtonIndex::try_from(grab.code)?,
+                                     grab.modifiers)?.check()?;
+                }
+
+            }
+        }
+
+        debug!("{}", function_name!());
+
+        Ok(())
+    }
+
+    pub(crate) fn unset(subtle: Subtle, win: Window) -> Result<()> {
+        let conn = subtle.conn.get().context("Failed to get connection")?;
+
+        let default_screen = &conn.setup().roots[subtle.screen_num];
+
+        // pub const XCB_GRAB_ANY: xcb_grab_t = 0;
+        conn.ungrab_key(Keycode::from(0), win, ModMask::ANY)?.check()?;
+        conn.ungrab_button(ButtonIndex::ANY, win, ModMask::ANY)?.check()?;
+
+        // Bind click-to-focus grab
+        if subtle.flags.intersects(SubtleFlags::FOCUS_CLICK) && default_screen.root != win {
+            conn.grab_button(false, win,
+                             EventMask::BUTTON_PRESS | EventMask::BUTTON_RELEASE,
+                             GrabMode::ASYNC, GrabMode::ASYNC, NONE, NONE,
+                             ButtonIndex::ANY, ModMask::ANY)?.check()?;
+        }
+
+
+        debug!("{}", function_name!());
+
+        Ok(())
+    }
 }
 
 impl fmt::Display for Grab {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "(code={}, state={}, app={:?})", self.code, self.state, self.app)
+        write!(f, "(code={}, state={:?}, app={:?})", self.code, self.modifiers, self.app)
     }
 }
 
 pub(crate) fn init(config: &Config, subtle: &mut Subtle) -> Result<()> {
+    let conn = subtle.conn.get().context("Failed to get connection")?;
+
+    // Get modifier mask
+    let reply = conn.get_modifier_mapping()?.reply()?;
+
+    println!("keycodes: {:?}", reply.keycodes);
+
+    // Parse grabs
     subtle.grabs = config.grabs.iter()
         .map(|grab| Grab::new(grab.0, grab.1))
         .filter_map(|res| res.ok()).collect();
 
-    // Check grabs
     if 0 == subtle.gravities.len() {
         return Err(anyhow!("No grabs found"));
     }
