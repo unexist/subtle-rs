da59c1a07ea2d6a64adfb341e59208e05924b5a8
Christoph Kappel <christoph@unexist.dev>
2025-12-17 12:10:20 +0100

    Added fuzzytime handling

--- plugins/fuzzytime/fuzzytime.go
+++ plugins/fuzzytime/fuzzytime.go
@@ -2,21 +2,48 @@ package main
 
 import (
 	"github.com/extism/go-pdk"
+	"strconv"
+	"strings"
+	"math"
 )
+
+var numbers = []string {
+	 "one", "two", "three", "four", "five", "six",
+	 "seven", "eight", "nine", "ten", "eleven", "twelve",
+}
+
+var text = []string {
+	 "%0 o'clock", "five past %0", "ten past %0", "quarter past %0",
+	 "twenty past %0", "twenty-five past %0", "half past %0",
+	 "twenty-five to %1", "twenty to %1", "quarter to %1", "ten to %1",
+	 "five to %1", "%1 o'clock",
+}
 
 //go:wasmimport extism:host/user get_formatted_time
 func get_formatted_time(uint64) uint64
 
 //go:export run
 func Run() int32 {
-	format := "[hour]:[minute]:[second]"
+	format := "[hour repr:12]:[minute]"
 	mem := pdk.AllocateString(format)
 	defer mem.Free()
 
 	ptr := get_formatted_time(mem.Offset())
 	rmem := pdk.FindMemory(ptr)
 
-	pdk.OutputString(string(rmem.ReadBytes()))
+	fields := strings.Split(string(rmem.ReadBytes()), ":")
+
+	hour, _ := strconv.Atoi(fields[0])
+	minute, _ := strconv.Atoi(fields[1])
+
+	result := strings.ReplaceAll(text[int(math.Round(float64(minute) / 5))], "%0", numbers[hour - 1])
+
+	cur_hour := hour
+	if 12 == hour { cur_hour = 0 }
+
+	result = strings.ReplaceAll(result, "%1", numbers[cur_hour])
+
+	pdk.OutputString(result)
 
 	return 0
 }
