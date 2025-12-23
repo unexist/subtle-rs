229cfd417f1473984711e651fc3d05f92411bf2f
Christoph Kappel <christoph@unexist.dev>
2025-12-23 14:13:13 +0100

    Added doc header

--- plugins/fuzzytime/fuzzytime.go
+++ plugins/fuzzytime/fuzzytime.go
@@ -1,3 +1,14 @@
+//
+// @package subtle-rs
+//
+// @file Fuzzytime plugin functions
+// @copyright 2025-present Christoph Kappel <christoph@unexist.dev>
+// @version $Id$
+//
+// This program can be distributed under the terms of the GNU GPLv3.
+// See the file LICENSE for details.
+//
+
 package main
 
 import (
--- plugins/time/src/time.zig
+++ plugins/time/src/time.zig
@@ -1,3 +1,14 @@
+//
+// @package subtle-rs
+//
+// @file Time plugin functions
+// @copyright 2025-present Christoph Kappel <christoph@unexist.dev>
+// @version $Id$
+//
+// This program can be distributed under the terms of the GNU GPLv3.
+// See the file LICENSE for details.
+//
+
 const std = @import("std");
 const extism_pdk = @import("extism-pdk");
 
