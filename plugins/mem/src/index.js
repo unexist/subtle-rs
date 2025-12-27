49f009d14c3a0e5b08251a70d4a86540eed0c2fe
Christoph Kappel <christoph@unexist.dev>
2025-12-27 21:31:54 +0100

    Updated handling of return values

--- plugins/mem/src/index.js
+++ plugins/mem/src/index.js
@@ -3,7 +3,9 @@ const { get_memory } = Host.getFunctions()
 export function run() {
   const ptr = get_memory();
   const rmem = Memory.find(ptr);
-  const mem = rmem.readString();
 
-  Host.outputString(`${mem}`)
+  const mem = rmem.readString().split(", ")
+      .flatMap((v) => Math.round(parseInt(v) / 1024 / 1024));
+
+  Host.outputString(`${mem[0]}g / ${mem[2]}g`)
 }
