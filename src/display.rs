b86141768185d920b9ddf53b830b80c60d7ba5af
Christoph Kappel <christoph@unexist.dev>
2026-01-04 13:25:55 +0100

    Added missing (c) symbol

--- src/display.rs
+++ src/display.rs
@@ -2,7 +2,7 @@
 /// @package subtle-rs
 ///
 /// @file Display functions
-/// @copyright 2025-present Christoph Kappel <christoph@unexist.dev>
+/// @copyright (c) 2025-present Christoph Kappel <christoph@unexist.dev>
 /// @version $Id$
 ///
 /// This program can be distributed under the terms of the GNU GPLv3.
