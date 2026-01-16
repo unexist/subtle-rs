a70928eadc191ace7659f2c45369140a4195bc5d
Christoph Kappel <christoph@unexist.dev>
2026-01-16 11:21:01 +0100

    Updated handling of format

--- plugins/mem/src/index.js
+++ plugins/mem/src/index.js
@@ -4,7 +4,7 @@ export function run() {
   const ptr = get_memory();
   const rmem = Memory.find(ptr);
 
-  const mem = rmem.readString().split(", ")
+  const mem = rmem.readString().split(" ")
       .flatMap((v) => Math.round(parseInt(v) / 1024 / 1024));
 
   Host.outputString(`${mem[0]}g / ${mem[2]}g`)
